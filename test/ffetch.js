b1f2053d303be3ba2db32792cad2fff2d7f48713
Dirk Rudolph <drudolph@adobe.com>
2023-03-02 13:31:10 +0100

    chore: test restructure

--- test/ffetch.js
+++ test/ffetch.js
@@ -36,21 +36,6 @@ describe('ffetch', () => {
     assert.equal(3, requestCount);
   });
 
-  describe('chunks', () => {
-
-    it('returns a generator for all entries with custom chunk size', async () => {
-      const entries = ffetch('/555-simple-entries.json', fetch).chunks(1000);
-      let i = 0;
-      for await (const entry of entries) {
-        assert.deepStrictEqual(entry, { title: `Entry ${i}` });
-        i += 1;
-      }
-      assert.equal(555, i);
-      assert.equal(1, requestCount);
-    });
-
-  })
-
   describe('failure hanlding', () => {
 
     it('returns an empty generator for a 404', async () => {
@@ -71,141 +56,160 @@ describe('ffetch', () => {
 
   })
 
-  describe('map', () => {
+  describe('operations', () => {
 
-    it('returns a generator that maps each entry', async () => {
-      const entries = ffetch('/555-simple-entries.json', fetch)
-        .map(({ title }) => title);
-      let i = 0;
-      for await (const entry of entries) {
-        assert.equal(entry, `Entry ${i}`);
-        i += 1;
-      }
-      assert.equal(555, i);
-    });
+    describe('chunks', () => {
 
-    it('returns the first enrty after applying multiple mappings', async () => {
-      const entry = await ffetch('/555-simple-entries.json', fetch)
-        .map(({ title }) => title)
-        .map(title => title.toUpperCase())
-        .first();
+      it('returns a generator for all entries with custom chunk size', async () => {
+        const entries = ffetch('/555-simple-entries.json', fetch).chunks(1000);
+        let i = 0;
+        for await (const entry of entries) {
+          assert.deepStrictEqual(entry, { title: `Entry ${i}` });
+          i += 1;
+        }
+        assert.equal(555, i);
+        assert.equal(1, requestCount);
+      });
 
-      assert.equal(entry, 'ENTRY 0');
-    });
+    })
 
-  })
+    describe('map', () => {
 
-  describe('filter', () => {
+      it('returns a generator that maps each entry', async () => {
+        const entries = ffetch('/555-simple-entries.json', fetch)
+          .map(({ title }) => title);
+        let i = 0;
+        for await (const entry of entries) {
+          assert.equal(entry, `Entry ${i}`);
+          i += 1;
+        }
+        assert.equal(555, i);
+      });
 
-    it('returns a generator that filters entries', async () => {
-      const expectedEntries = ['Entry 99', 'Entry 199', 'Entry 299', 'Entry 399', 'Entry 499'];
-      const entries = ffetch('/555-simple-entries.json', fetch)
-        .filter(({ title }) => expectedEntries.indexOf(title) >= 0);
-      let i = 0;
-      for await (const entry of entries) {
-        assert.equal(entry.title, expectedEntries[i]);
-        i += 1;
-      }
-      assert.equal(5, i);
-    });
+      it('returns the first enrty after applying multiple mappings', async () => {
+        const entry = await ffetch('/555-simple-entries.json', fetch)
+          .map(({ title }) => title)
+          .map(title => title.toUpperCase())
+          .first();
 
-    it('returns the first enrty after multiple filters', async () => {
-      const entry = await ffetch('/555-simple-entries.json', fetch)
-        .filter(({ title }) => title.indexOf('9') > 0)
-        .filter(({ title }) => title.indexOf('8') > 0)
-        .filter(({ title }) => title.indexOf('4') > 0)
-        .first();
+        assert.equal(entry, 'ENTRY 0');
+      });
 
-      assert.deepStrictEqual(entry, { title: 'Entry 489' });
-    });
+    })
 
-  })
+    describe('filter', () => {
 
-  describe('limit', () => {
+      it('returns a generator that filters entries', async () => {
+        const expectedEntries = ['Entry 99', 'Entry 199', 'Entry 299', 'Entry 399', 'Entry 499'];
+        const entries = ffetch('/555-simple-entries.json', fetch)
+          .filter(({ title }) => expectedEntries.indexOf(title) >= 0);
+        let i = 0;
+        for await (const entry of entries) {
+          assert.equal(entry.title, expectedEntries[i]);
+          i += 1;
+        }
+        assert.equal(5, i);
+      });
 
-    it('returns a generator for a limited set entries', async () => {
-      const entries = ffetch('/555-simple-entries.json', fetch)
-        .limit(10);
-      let i = 0;
-      for await (const entry of entries) {
-        assert.deepStrictEqual(entry, { title: `Entry ${i}` });
-        i += 1;
-      }
-      assert.equal(10, i);
-    });
+      it('returns the first enrty after multiple filters', async () => {
+        const entry = await ffetch('/555-simple-entries.json', fetch)
+          .filter(({ title }) => title.indexOf('9') > 0)
+          .filter(({ title }) => title.indexOf('8') > 0)
+          .filter(({ title }) => title.indexOf('4') > 0)
+          .first();
 
-    it('returns an array of all entries', async () => {
-      const entries = await ffetch('/555-simple-entries.json', fetch)
-        .limit(5)
-        .all();
+        assert.deepStrictEqual(entry, { title: 'Entry 489' });
+      });
 
-      assert.deepStrictEqual(entries, [
-        { title: 'Entry 0' },
-        { title: 'Entry 1' },
-        { title: 'Entry 2' },
-        { title: 'Entry 3' },
-        { title: 'Entry 4' },
-      ]);
-    });
+    })
 
-  })
+    describe('limit', () => {
 
-  describe('slice', () => {
+      it('returns a generator for a limited set entries', async () => {
+        const entries = ffetch('/555-simple-entries.json', fetch)
+          .limit(10);
+        let i = 0;
+        for await (const entry of entries) {
+          assert.deepStrictEqual(entry, { title: `Entry ${i}` });
+          i += 1;
+        }
+        assert.equal(10, i);
+      });
 
-    it('returns a generator that filters a sliced set of entries', async () => {
-      const expectedEntries = ['Entry 99', 'Entry 199', 'Entry 299', 'Entry 399', 'Entry 499'];
-      const entries = ffetch('/555-simple-entries.json', fetch)
-        .filter(({ title }) => expectedEntries.indexOf(title) >= 0)
-        .slice(2, 4);
-      let i = 0;
-      for await (const entry of entries) {
-        assert.equal(entry.title, expectedEntries[i + 2]);
-        i += 1;
-      }
-      assert.equal(2, i);
-    });
+      it('returns an array of all entries', async () => {
+        const entries = await ffetch('/555-simple-entries.json', fetch)
+          .limit(5)
+          .all();
 
-    it('returns an array of a slice of entries', async () => {
-      const entries = await ffetch('/555-simple-entries.json', fetch)
-        .slice(300, 305)
-        .all();
+        assert.deepStrictEqual(entries, [
+          { title: 'Entry 0' },
+          { title: 'Entry 1' },
+          { title: 'Entry 2' },
+          { title: 'Entry 3' },
+          { title: 'Entry 4' },
+        ]);
+      });
 
-      assert.deepStrictEqual(entries, [
-        { title: 'Entry 300' },
-        { title: 'Entry 301' },
-        { title: 'Entry 302' },
-        { title: 'Entry 303' },
-        { title: 'Entry 304' },
-      ]);
+    })
 
-      assert.equal(2, requestCount);
-    });
+    describe('slice', () => {
 
-  })
+      it('returns a generator that filters a sliced set of entries', async () => {
+        const expectedEntries = ['Entry 99', 'Entry 199', 'Entry 299', 'Entry 399', 'Entry 499'];
+        const entries = ffetch('/555-simple-entries.json', fetch)
+          .filter(({ title }) => expectedEntries.indexOf(title) >= 0)
+          .slice(2, 4);
+        let i = 0;
+        for await (const entry of entries) {
+          assert.equal(entry.title, expectedEntries[i + 2]);
+          i += 1;
+        }
+        assert.equal(2, i);
+      });
 
-  describe('follow', () => {
-    it('returns the html parsed as document when following a reference', async () => {
-      const entry = await ffetch('/one-entry-with-a-reference.json', fetch, parseDocument)
-        .follow('path')
-        .first();
+      it('returns an array of a slice of entries', async () => {
+        const entries = await ffetch('/555-simple-entries.json', fetch)
+          .slice(300, 305)
+          .all();
 
-      assert(entry);
-    });
+        assert.deepStrictEqual(entries, [
+          { title: 'Entry 300' },
+          { title: 'Entry 301' },
+          { title: 'Entry 302' },
+          { title: 'Entry 303' },
+          { title: 'Entry 304' },
+        ]);
 
-    it('returns null if the reference does not exist', async () => {
-      const entry = await ffetch('/one-entry-with-a-reference.json', fetch, parseDocument)
-        .follow('reference')
-        .first();
+        assert.equal(2, requestCount);
+      });
 
-      assert(!entry);
-    });
+    })
 
-    it('returns null if the referenced document is not found', async () => {
-      const entry = await ffetch('/one-entry-with-a-none-existing-reference.json', fetch, parseDocument)
-        .follow('reference')
-        .first();
+    describe('follow', () => {
+      it('returns the html parsed as document when following a reference', async () => {
+        const entry = await ffetch('/one-entry-with-a-reference.json', fetch, parseDocument)
+          .follow('path')
+          .first();
 
-      assert(!entry);
-    });
+        assert(entry);
+      });
+
+      it('returns null if the reference does not exist', async () => {
+        const entry = await ffetch('/one-entry-with-a-reference.json', fetch, parseDocument)
+          .follow('reference')
+          .first();
+
+        assert(!entry);
+      });
+
+      it('returns null if the referenced document is not found', async () => {
+        const entry = await ffetch('/one-entry-with-a-none-existing-reference.json', fetch, parseDocument)
+          .follow('reference')
+          .first();
+
+        assert(!entry);
+      });
+    })
+
   })
 });

a212c82b1bf8eb0edfc8bfd576b66dec34d04c4c
Dirk Rudolph <drudolph@adobe.com>
2023-09-06 08:12:23 +0200

    fix: eslint based on a project's configuration (#8)
    * fix: eslint ffetch.js
    
    * ci: add workflow to run lint and test
    
    * fix: liniting issues
    
    * fix: liniting issues
    
    * fix: use project eslint config
    
    ---------
    
    Co-authored-by: Aravindo Wingeier <synox@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,35 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 module.exports = {
+  root: true,
+  extends: 'airbnb-base',
   env: {
     browser: true,
-    es2021: true,
   },
-  extends: '@adobe/helix',
-  overrides: [
-  ],
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest',
+    allowImportExportEverywhere: true,
     sourceType: 'module',
+    requireConfigFile: false,
   },
   rules: {
-    'no-await-in-loop': 'off',
-    'no-shadow': 'off'
+    // allow reassigning param
+    'no-param-reassign': [2, { props: false }],
+    'linebreak-style': ['error', 'unix'],
+    'import/extensions': ['error', {
+      js: 'always',
+    }],
+    'no-use-before-define': 'off',
+    'no-restricted-syntax': 'off',
   },
-};
+}
